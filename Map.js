0f3ef4562883b851d14c60dee9e395f0e9f9f7c7
webpojectitera123 <106723757+webpojectitera123@users.noreply.github.com>
2022-06-23 16:54:26 +0700

    Create Map.js

---
+++ Map.js
@@ -0,0 +1,48 @@
+import { React, useEffect, useState } from "react";
+import { LayersControl, MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import "leaflet-easybutton/src/easy-button.js";
+import "leaflet-easybutton/src/easy-button.css";
+import "font-awesome/css/font-awesome.min.css";
+
+const { BaseLayer } = LayersControl;
+
+export default function Home() {
+  const [map, setMap] = useState(null);
+  const [position, setPosition] = useState(null);
+
+  useEffect(() => {
+    if (!map) return;
+
+    L.easyButton("fa-map-marker", () => {
+      map.locate().on("locationfound", function (e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+      });
+    }).addTo(map);
+  }, [map]);
+
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={20}
+      style={{ height: "100vh" }}
+      whenCreated={setMap}
+    >
+      <LayersControl>
+        <BaseLayer checked name="OpenStreetMap">
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png "
+          />
+        </BaseLayer>
+      </LayersControl>
+    </MapContainer>
+  );
+}
+
+
+// install package
+
+// npm install font-awesome leaflet leaflet-easybutton react-lifecycles-compat
